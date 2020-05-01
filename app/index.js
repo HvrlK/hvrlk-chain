b457df9c2afa7ba168a2754e85ebd42c98c133a0
Vitalii Havryliuk <v.havryliuk@noisyminer.com>
2020-05-01 13:31:43 +0300

    Create the Blockchain Network

--- app/index.js
+++ app/index.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const P2pServer = require('./p2p-server');
 const Blockchain = require('../blockchain');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app =  express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
 
 app.post('/mine', (req, res) => {
   const block = bc.addBlock(req.body.data);
   console.log(`New block added: ${block.toString()}`)
+
+  p2pServer.syncChains();
 
   res.redirect('/blocks');
 });
@@ -21,3 +25,4 @@ app.get('/blocks', (req, res) => {
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+p2pServer.listen();
